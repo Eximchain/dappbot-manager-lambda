2f711fdddc02602eaac8b931d011e4c93ae7f9dc
John O'Sullivan <john@eximchain.com>
2019-03-27 16:11:02 -0400

    Updated Dappsmith

--- node_modules/@eximchain/abi2reducks/dist/util.js
+++ node_modules/@eximchain/abi2reducks/dist/util.js
@@ -11,6 +11,7 @@
     Object.defineProperty(exports, "__esModule", { value: true });
     var fs = require('fs-extra');
     var path = require('path');
+    var shell = require('shelljs');
     var handlebars_1 = require("./handlebars");
     exports.loadTemplate = function (templatePath) {
         return String(fs.readFileSync(path.resolve(__dirname, templatePath)));
@@ -19,6 +20,22 @@
         if (templateArg === void 0) { templateArg = {}; }
         var templateCode = handlebars_1.default.compile(exports.loadTemplate(templatePath))(templateArg);
         fs.writeFileSync(filePath, templateCode);
+    };
+    exports.writeTxDuck = function (method) {
+        var dirName = handlebars_1.camelCase(method.name);
+        fs.ensureDirSync(dirName);
+        shell.cd(dirName);
+        var templateArg = {
+            methodName: method.name,
+            methodAbi: method,
+            titleName: handlebars_1.pascalCase(method.name)
+        };
+        exports.writeTemplateToPath('./templates/duck/index.hbs', './index.ts', templateArg);
+        exports.writeTemplateToPath('./templates/duck/actions.hbs', './actions.ts', templateArg);
+        exports.writeTemplateToPath('./templates/duck/reducers.hbs', './reducers.ts', templateArg);
+        exports.writeTemplateToPath('./templates/duck/selectors.hbs', './selectors.ts', templateArg);
+        exports.writeTemplateToPath('./templates/duck/types.hbs', './types.ts', templateArg);
+        shell.cd('..');
     };
 });
 //# sourceMappingURL=util.js.map
