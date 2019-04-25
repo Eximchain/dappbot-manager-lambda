44de33a14c6d03fd3d970d6c1e75bf172c75737f
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-04-25 17:23:58 -0400

    Change validate to use assert module

--- validate.js
+++ validate.js
@@ -4,36 +4,20 @@ const assert = require('assert');
 const dappLimitAttrName = 'dev:custom:num_dapps';
 
 function validateBodyDelete(body) {
-    if (!body.hasOwnProperty('DappName')) {
-        throw new Error("delete: required argument 'DappName' not found");
-    }
+    assert(body.hasOwnProperty('DappName'), "delete: required argument 'DappName' not found");
 }
 
 function validateBodyRead(body) {
-    if (!body.hasOwnProperty('DappName')) {
-        throw new Error("read: required argument 'DappName' not found");
-    }
+    assert(body.hasOwnProperty('DappName'), "read: required argument 'DappName' not found");
 }
 
 function validateBodyCreate(body) {
-    if (!body.hasOwnProperty('DappName')) {
-        throw new Error("create: required argument 'DappName' not found");
-    }
-    if (!body.hasOwnProperty('OwnerEmail')) {
-        throw new Error("create: required argument 'OwnerEmail' not found");
-    }
-    if (!body.hasOwnProperty('Abi')) {
-        throw new Error("create: required argument 'Abi' not found");
-    }
-    if (!body.hasOwnProperty('ContractAddr')) {
-        throw new Error("create: required argument 'ContractAddr' not found");
-    }
-    if (!body.hasOwnProperty('Web3URL')) {
-        throw new Error("create: required argument 'Web3URL' not found");
-    }
-    if (!body.hasOwnProperty('GuardianURL')) {
-        throw new Error("create: required argument 'GuardianURL' not found");
-    }
+    assert(body.hasOwnProperty('DappName'), "create: required argument 'DappName' not found");
+    assert(body.hasOwnProperty('OwnerEmail'), "create: required argument 'OwnerEmail' not found");
+    assert(body.hasOwnProperty('Abi'), "create: required argument 'Abi' not found");
+    assert(body.hasOwnProperty('ContractAddr'), "create: required argument 'ContractAddr' not found");
+    assert(body.hasOwnProperty('Web3URL'), "create: required argument 'Web3URL' not found");
+    assert(body.hasOwnProperty('GuardianURL'), "create: required argument 'GuardianURL' not found");
 }
 
 async function validateLimitsCreate(cognitoUsername, ownerEmail) {
