daf0a68d7bedda7892690b7b045b4801e1ef5727
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-05-01 15:45:51 -0400

    Better documentation on promise retry functions

--- common.js
+++ common.js
@@ -16,16 +16,32 @@ function dappNameTag(dappName) {
     }
 }
 
+/*
+Returns a Promise that rejects with reason after msDelay milliseconds
+*/
 function rejectDelay(reason) {
-    let t = 500;
+    let msDelay = 700;
     return new Promise(function(resolve, reject) {
-        setTimeout(reject.bind(null, reason), t); 
+        setTimeout(reject.bind(null, reason), msDelay); 
     });
 }
 
+/*
+Retries a promise returned by promiseGenerator up to maxRetries times as long as the error is retryable
+Based on https://stackoverflow.com/questions/38213668/promise-retry-design-patterns
+*/
 function retryPromise(promiseGenerator, maxRetries) {
+    // Ensure we call promiseGenerator on the first iteration
     let p = Promise.reject({retryable: true});
 
+    /*
+    Run retry and wait loop up to maxRetries iterations until promiseGenerator returns a promise that resolves
+
+    1. As long as promiseGenerator() rejects with a retryable error, we retry and then delay before the next loop iteration
+    2. If promiseGenerator() resolves, the rest of the loop will finish without triggering any further catch functions
+    3. If promiseGenerator() rejects with a non-retryable error, the rest of the loop will finish without any further
+       retries or delays since all catch blocks will simply return Promise.reject(err)
+    */
     for(var i=0; i<maxRetries; i++) {
         p = p.catch(err => err.retryable ? promiseGenerator() : Promise.reject(err))
              .catch(err => err.retryable ? rejectDelay(err) : Promise.reject(err));
