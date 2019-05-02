4f5e797bbf2b3f5c66e2e806bc32c2f2a25369a5
John O'Sullivan <john@eximchain.com>
2019-05-02 12:18:45 -0400

    Rewrote api handlers to use async/await and try/catch

--- api.js
+++ api.js
@@ -1,286 +1,152 @@
 const { dynamoDB, route53, cloudfront, s3, codepipeline } = require('./services');
 const validate = require('./validate');
 
+const logErr = (stage, err) => { console.log(`Error on ${stage}: `, err) }
+const logSafeErr = (stage, reason) => { console.log(`Safe error during ${stage}: ${reason}`) }
+const logSuccess = (stage, res) => { console.log(`Successfully completed ${stage}; result: `, res) }
+
+const successResponse = (body) => {
+    let responseCode = 200;
+    // TODO: Replace with something useful or remove
+    let responseHeaders = {"x-custom-header" : "my custom header value"};
+    return {
+        statusCode: responseCode,
+        headers: responseHeaders,
+        body: JSON.stringify(body)
+    };
+}
+
+// Using this factory function lets us create a new "stage" variable
+// for each invocation.  Otherwise, `stage` and `callAndLog` function would
+// need to be re-declared in each of the functions below.
+const callFactory = (startStage) => {
+    let stage = startStage;
+    const callAndLog = async (newStage, promise) => {
+        stage = newStage;
+        let res = await promise;
+        logSuccess(stage, res);
+        return res;
+    }
+    return [stage, callAndLog];
+}
+
 async function apiCreate(body, owner) {
-    return new Promise(function(resolve, reject) {
-        let dappName = validate.cleanName(body.DappName);
-        let abi = body.Abi;
-        let web3URL = body.Web3URL;
-        let guardianURL = body.GuardianURL;
-        let addr = body.ContractAddr;
-        let bucketName = s3.newBucketName();
-        let s3Dns = null;
-        let cloudfrontDistroId = null;
-        let cloudfrontDns = null;
+    let dappName = validate.cleanName(body.DappName);
+    let abi = body.Abi;
+    let web3URL = body.Web3URL;
+    let guardianURL = body.GuardianURL;
+    let addr = body.ContractAddr;
+    let bucketName = s3.newBucketName();
+    let s3Dns = null;
+    let cloudfrontDistroId = null;
+    let cloudfrontDns = null;
+    let result = null;
 
-        s3.createBucketWithTags(bucketName, dappName, owner)
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error Create S3 Bucket", err);
-            err.handled = true;
-            throw err;
-        })
-        .then(function(result){
-            console.log("Create Bucket Success", result);
-            return s3.setBucketPublic(bucketName);
-        })
-        .then(function(result){
-            console.log("Set Bucket as public readable success")
-            return s3.configureBucketWebsite(bucketName);
-        })
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error Configure S3 Bucket", err);
-            err.handled = true;
-            throw err;
-        })
-        .then(function(result){
-            console.log("Configure Bucket as Static Site success", result)
-            return s3.putDappseed({
-                dappName, web3URL, guardianURL, abi, addr
-            });
-        })
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error Put Dappseed", err);
-            err.handled = true;
-            throw err;
-        })
-        .then(function(result) {
-            console.log("Put Dappseed success", result);
-            return codepipeline.create(dappName, bucketName)
-        })
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error Create CodePipeline", err);
-            err.handled = true;
-            throw err;
-        })
-        .then(function(result) {
-            console.log("Create CodePipeline success", result);
-            s3Dns = s3.bucketEndpoint(bucketName);
-            return cloudfront.createDistro(dappName, owner, s3Dns);
-        })
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error Create CloudFront Distribution", err);
-            err.handled = true;
-            throw err;
-        })
-        .then(function(result) {
-            console.log("Create Cloudfront Distribution Success", result);
-            cloudfrontDistroId = result.Distribution.Id;
-            cloudfrontDns = result.Distribution.DomainName;
-            return route53.createRecord(dappName, cloudfrontDns);
-        })
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error Create Route53 Record", err);
-            err.handled = true;
-            throw err;
-        })
-        .then(function(result) {
-            console.log("Create DNS Record Success", result);
-            // TODO: Put custom dns instead
-            return dynamoDB.putItem(dappName, owner, abi, bucketName, cloudfrontDistroId, cloudfrontDns);
-        })
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error Put Dapp Item", err);
-            err.handled = true;
-            throw err;
-        })
-        .then(function(result) {
-            console.log("Dapp generation successfully initialized!  Check your URL in about 20 minutes.", result);
-            let responseCode = 200;
-            // TODO: Replace with something useful or remove
-            let responseHeaders = {"x-custom-header" : "my custom header value"};
+    let [stage, callAndLog] = callFactory('Pre-Creation');
 
-            let responseBody = {
-                method: "create"
-            };
-            let response = {
-                statusCode: responseCode,
-                headers: responseHeaders,
-                body: JSON.stringify(responseBody)
-            };
-            resolve(response);
-        })
-        .catch(function(err) {
-            console.log("Error", err);
-            reject(err);
-        })
-    });
+    try {
+        await callAndLog('Create S3 Bucket', s3.createBucketWithTags(bucketName, dappName, owner))
+        await callAndLog('Set Bucket Readable', s3.setBucketPublic(bucketName));
+        await callAndLog('Configure Bucket Website', s3.configureBucketWebsite(bucketName))
+        await callAndLog('Put DappSeed', s3.putDappseed({
+            dappName, web3URL, guardianURL, abi, addr
+        }));
+        await callAndLog('Create CodePipeline', codepipeline.create(dappName, bucketName));
+        s3Dns = s3.bucketEndpoint(bucketName);
+        result = await callAndLog('Create Cloudfront Distro', cloudfront.createDistro(dappName, owner, s3Dns));
+        cloudfrontDistroId = result.Distribution.Id;
+        cloudfrontDns = result.Distribution.DomainName;
+        await callAndLog('Create Route 53 record', route53.createRecord(dappName, cloudfrontDns));
+        await callAndLog('Create DynamoDB record', dynamoDB.putItem(dappName, owner, abi, bucketName, cloudfrontDistroId, cloudfrontDns));
+        console.log("Dapp generation successfully initialized!", result);
+
+        let responseBody = {
+            method: "create",
+            message: "Dapp generation successfully initialized!  Check your URL in about 5 minutes."
+        };
+        return successResponse(responseBody)
+    } catch (e) {
+        logErr(stage, e);
+        throw e;
+    }
 }
 
 async function apiRead(body) {
-    return new Promise(function(resolve, reject) {
-        let dappName = validate.cleanName(body.DappName);
-        dynamoDB.getItem(dappName)
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error Get Dapp Item", err);
-            err.handled = true;
-            throw err;
-        })
-        .then(function(result){
-            console.log("Get Dapp Item Success", result);
-            let responseCode = 200;
-            // TODO: Replace with something useful or remove
-            let responseHeaders = {"x-custom-header" : "my custom header value"};
+    let dappName = validate.cleanName(body.DappName);
 
-            let responseBody = {
-                method: "read",
-                item: result.Item
-            };
-            let response = {
-                statusCode: responseCode,
-                headers: responseHeaders,
-                body: JSON.stringify(responseBody)
-            };
-            resolve(response);
-        })
-        .catch(function(err) {
-            console.log("Error", err);
-            reject(err);
-        })   
-    });
+    let [stage, callAndLog] = callFactory('Pre-Read');
+
+    try {
+        const dappItem = await callAndLog('Get DynamoDB Item', dynamoDB.getItem(dappName));
+        let responseBody = {
+            method: "read",
+            item: dappItem.Item
+        };
+        return successResponse(responseBody);
+    } catch {
+        logErr(stage, e);
+    }
 }
 
-// TODO: Make sure incomplete steps are cleaned up
-async function apiDelete(body) {
-    return new Promise(function(resolve, reject) {
-        let dappName = validate.cleanName(body.DappName);
-        let bucketName = null;
-        let cloudfrontDistroId = null;
-        let cloudfrontDns = null;
 
-        dynamoDB.getItem(dappName)
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error Get Dapp Item", err);
-            err.handled = true;
-            throw err;
-        })
-        .then(function(result){
-            console.log("Get Dapp Item Success", result);
-            bucketName = result.Item.S3BucketName.S;
-            cloudfrontDistroId = result.Item.CloudfrontDistributionId.S;
-            cloudfrontDns = result.Item.CloudfrontDnsName.S;
-            return cloudfront.disableDistro(cloudfrontDistroId);
-        })
-        .then(function(result) {
-            console.log("Cloudfront Disable Success", result);
-            return Promise.resolve("TODO: Cloudfront's delete is turned off until we have a working strategy.");
-            // return cloudfront.deleteDistro(cloudfrontDistroId);
-        })
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error Cloudfront Delete", err);
-            err.handled = true;
+async function apiDelete(body){
+    let dappName = validate.cleanName(body.DappName);
+    let bucketName = null;
+    let cloudfrontDistroId = null;
+    let cloudfrontDns = null;
+
+    let [stage, callAndLog] = callFactory('Pre-Delete');
+
+    try {
+        const dbItem = await callAndLog('Get Dapp DynamoDb Item', dynamoDB.getItem(dappName));
+        bucketName = dbItem.Item.S3BucketName.S;
+        cloudfrontDistroId = dbItem.Item.CloudfrontDistributionId.S;
+        cloudfrontDns = dbItem.Item.CloudfrontDnsName.S;
+        await callAndLog('Disable Cloudfront distro', cloudfront.disableDistro(cloudfrontDistroId));
+        
+        try {
+            await callAndLog('Delete Cloudfront distro', Promise.resolve("TODO: Cloudfront's delete is turned off until we have a working strategy."));
+        } catch (err) {
             switch(err.code) {
                 case 'NoSuchDistribution':
-                    return Promise.resolve("Distribution already deleted");
+                    logSafeErr("Distribution already deleted.")
+                    break;
+                default:
+                    logErr(stage, err);
+                    throw err;
             }
-            throw err;
-        })
-        .then(function(result) {
-            console.log("Cloudfront Delete Success", result);
-            return route53.deleteRecord(dappName, cloudfrontDns);
-        })
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error Delete Route53 Record", err);
-            err.handled = true;
-            throw err;
-        })
-        .then(function(result) {
-            console.log("Delete DNS Record Success", result);
-            return s3.emptyBucket(bucketName);
-        })
-        .then(function(result) {
-            console.log("S3 Bucket Empty Success", result);
-            return s3.deleteBucket(bucketName);
-        })
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error S3 Bucket Delete", err);
-            err.handled = true;
+        }
+
+        await callAndLog('Delete Route53 Record', route53.deleteRecord(dappName, cloudfrontDns));
+
+        try {
+            await callAndLog('Empty S3 Bucket', s3.emptyBucket(bucketName));
+            await callAndLog('Delete S3 Bucket', s3.deleteBucket(bucketName));
+        } catch (err) {
             switch(err.code) {
                 case 'NoSuchBucket':
-                    return Promise.resolve("Bucket already deleted");
+                    logSafeErr(stage, 'Bucket already deleted.');
+                    break;
+                default:
+                    logErr(stage, err);
+                    throw err;
             }
-            throw err;
-        })
-        .then(function(result) {
-            console.log("S3 Bucket Delete Success", result);
-            return dynamoDB.deleteItem(dappName);
-        })
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error Delete Dapp Item", err);
-            err.handled = true;
-            throw err;
-        })
-        .then(function(result) {
-            console.log("Delete Dapp Item Success", result);
-            return codepipeline.delete(dappName);
-        })
-        .catch(function(err) {
-            if (err.handled) {
-                throw err;
-            }
-            console.log("Error Delete CodePipeline", err);
-            err.handled = true;
-            throw err;
-        })
-        .then(function(result) {
-            console.log("Delete CodePipeline Success", result);
-            let responseCode = 200;
-            // TODO: Replace with something useful or remove
-            let responseHeaders = {"x-custom-header" : "my custom header value"};
+        }
 
-            let responseBody = {
-                method: "delete"
-            };
-            let response = {
-                statusCode: responseCode,
-                headers: responseHeaders,
-                body: JSON.stringify(responseBody)
-            };
-            resolve(response);
-        })
-        .catch(function(err) {
-            console.log("Error", err);
-            reject(err);
-        })
-    });
+        await callAndLog('Delete DynamoDB item', dynamoDB.deleteItem(dappName));
+        await callAndLog('Delete CodePipeline', codepipeline.delete(dappName));
+
+        let responseBody = {
+            method: "delete",
+            message: "Your Dapp was successfully deleted."
+        };
+        return successResponse(responseBody);
+
+    } catch (e) {
+        logErr(stage, e);
+        throw e;
+    }
+
 }
 
 module.exports = {
