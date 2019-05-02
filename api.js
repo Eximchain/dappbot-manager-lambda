2ab0198f33ec946283e9f300badee4ab74ade515
John O'Sullivan <john@eximchain.com>
2019-05-02 14:09:27 -0400

    Converted some arrows to functions, added spacing, made es to errs, ensured we always return a response, improved some names

--- api.js
+++ api.js
@@ -2,24 +2,25 @@ const { dynamoDB, route53, cloudfront, s3, codepipeline } = require('./services'
 const validate = require('./validate');
 
 const logErr = (stage, err) => { console.log(`Error on ${stage}: `, err) }
-const logSafeErr = (stage, reason) => { console.log(`Safe error during ${stage}: ${reason}`) }
+const logNonFatalErr = (stage, reason) => { console.log(`Ignoring non-fatal error during ${stage}: ${reason}`) }
 const logSuccess = (stage, res) => { console.log(`Successfully completed ${stage}; result: `, res) }
 
-const successResponse = (body) => {
-    let responseCode = 200;
+function response(body, opts={isCreate : false}){
+    // HTTP 201 means a successful response which has resulted in resources being created
+    let responseCode = opts.isCreate ? 201 : 200;
     // TODO: Replace with something useful or remove
     let responseHeaders = {"x-custom-header" : "my custom header value"};
     return {
         statusCode: responseCode,
-        headers: responseHeaders,
+        header: responseHeaders,
         body: JSON.stringify(body)
-    };
+    }
 }
 
 // Using this factory function lets us create a new "stage" variable
 // for each invocation.  Otherwise, `stage` and `callAndLog` function would
 // need to be re-declared in each of the functions below.
-const callFactory = (startStage) => {
+function callFactory(startStage) {
     let stage = startStage;
     const callAndLog = async (newStage, promise) => {
         stage = newStage;
@@ -32,15 +33,14 @@ const callFactory = (startStage) => {
 
 async function apiCreate(body, owner) {
     let dappName = validate.cleanName(body.DappName);
+
     let abi = body.Abi;
     let web3URL = body.Web3URL;
     let guardianURL = body.GuardianURL;
     let addr = body.ContractAddr;
+
     let bucketName = s3.newBucketName();
-    let s3Dns = null;
-    let cloudfrontDistroId = null;
-    let cloudfrontDns = null;
-    let result = null;
+    let s3Dns = s3.bucketEndpoint(bucketName);
 
     let [stage, callAndLog] = callFactory('Pre-Creation');
 
@@ -48,26 +48,27 @@ async function apiCreate(body, owner) {
         await callAndLog('Create S3 Bucket', s3.createBucketWithTags(bucketName, dappName, owner))
         await callAndLog('Set Bucket Readable', s3.setBucketPublic(bucketName));
         await callAndLog('Configure Bucket Website', s3.configureBucketWebsite(bucketName))
-        await callAndLog('Put DappSeed', s3.putDappseed({
-            dappName, web3URL, guardianURL, abi, addr
-        }));
+        await callAndLog('Put DappSeed', s3.putDappseed({ dappName, web3URL, guardianURL, abi, addr }));
         await callAndLog('Create CodePipeline', codepipeline.create(dappName, bucketName));
-        s3Dns = s3.bucketEndpoint(bucketName);
-        result = await callAndLog('Create Cloudfront Distro', cloudfront.createDistro(dappName, owner, s3Dns));
-        cloudfrontDistroId = result.Distribution.Id;
-        cloudfrontDns = result.Distribution.DomainName;
+
+        const newDistro = await callAndLog('Create Cloudfront Distro', cloudfront.createDistro(dappName, owner, s3Dns));
+
+        let cloudfrontDistroId = newDistro.Distribution.Id;
+        let cloudfrontDns = newDistro.Distribution.DomainName;
+
         await callAndLog('Create Route 53 record', route53.createRecord(dappName, cloudfrontDns));
-        await callAndLog('Create DynamoDB record', dynamoDB.putItem(dappName, owner, abi, bucketName, cloudfrontDistroId, cloudfrontDns));
-        console.log("Dapp generation successfully initialized!", result);
+        await callAndLog('Create DynamoDB item', dynamoDB.putItem(dappName, owner, abi, bucketName, cloudfrontDistroId, cloudfrontDns));
+
+        console.log("Dapp generation successfully initialized!");
 
         let responseBody = {
             method: "create",
             message: "Dapp generation successfully initialized!  Check your URL in about 5 minutes."
         };
-        return successResponse(responseBody)
-    } catch (e) {
-        logErr(stage, e);
-        throw e;
+        return response(responseBody, {isCreate:true})
+    } catch (err) {
+        logErr(stage, err);
+        return response(err);
     }
 }
 
@@ -82,26 +83,25 @@ async function apiRead(body) {
             method: "read",
             item: dappItem.Item
         };
-        return successResponse(responseBody);
-    } catch {
-        logErr(stage, e);
+        return response(responseBody);
+    } catch (err) {
+        logErr(stage, err);
+        return response(err);
     }
 }
 
 
 async function apiDelete(body){
     let dappName = validate.cleanName(body.DappName);
-    let bucketName = null;
-    let cloudfrontDistroId = null;
-    let cloudfrontDns = null;
 
     let [stage, callAndLog] = callFactory('Pre-Delete');
 
     try {
         const dbItem = await callAndLog('Get Dapp DynamoDb Item', dynamoDB.getItem(dappName));
-        bucketName = dbItem.Item.S3BucketName.S;
-        cloudfrontDistroId = dbItem.Item.CloudfrontDistributionId.S;
-        cloudfrontDns = dbItem.Item.CloudfrontDnsName.S;
+
+        let bucketName = dbItem.Item.S3BucketName.S;
+        let cloudfrontDistroId = dbItem.Item.CloudfrontDistributionId.S;
+        let cloudfrontDns = dbItem.Item.CloudfrontDnsName.S;
         
         try {
             await callAndLog('Disable Cloudfront distro', cloudfront.disableDistro(cloudfrontDistroId));
@@ -109,7 +109,7 @@ async function apiDelete(body){
         } catch (err) {
             switch(err.code) {
                 case 'NoSuchDistribution':
-                    logSafeErr("Distribution already deleted.")
+                    logNonFatalErr("Distribution already deleted.")
                     break;
                 default:
                     logErr(stage, err);
@@ -125,7 +125,7 @@ async function apiDelete(body){
         } catch (err) {
             switch(err.code) {
                 case 'NoSuchBucket':
-                    logSafeErr(stage, 'Bucket already deleted.');
+                    logNonFatalErr(stage, 'Bucket already deleted.');
                     break;
                 default:
                     logErr(stage, err);
@@ -140,11 +140,11 @@ async function apiDelete(body){
             method: "delete",
             message: "Your Dapp was successfully deleted."
         };
-        return successResponse(responseBody);
+        return response(responseBody);
 
-    } catch (e) {
-        logErr(stage, e);
-        throw e;
+    } catch (err) {
+        logErr(stage, err);
+        return response(err, {isErr:true});
     }
 
 }
