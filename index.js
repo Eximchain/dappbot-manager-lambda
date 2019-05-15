118eee7db060172f406c9f803a413da2bff3f1cd
John O'Sullivan <john@eximchain.com>
2019-05-15 11:48:34 -0400

    Reduced nesting by extracting alternate and error cases to the top, kinda Go-style

--- index.js
+++ index.js
@@ -6,48 +6,50 @@ const validate = require('./validate');
 exports.handler = async (event) => {
     console.log("request: " + JSON.stringify(event));
 
+    // Pass CodePipeline events straight to cleanup function
+    if (event['CodePipeline.job']){
+        return cleanup.postPipelineCleanup(event['CodePipeline.job']);
+    }
+
     // Auto-return success for CORS pre-flight OPTIONS requests
     if (event.httpMethod && event.httpMethod.toLowerCase() == 'options'){
         return api.successResponse({});
     }
 
-    // Pass CodePipeline events to clean.js, others to api.js
     let responsePromise;
-    if (event['CodePipeline.job']){
-        return cleanup.postPipelineCleanup(event['CodePipeline.job']);
-    } else if (event.httpMethod){
-        let method = event.pathParameters.proxy;
-        let body = null;
-        if (event.body) {
-            body = JSON.parse(event.body);
-        }
-        let authorizedUser = event.requestContext.authorizer.claims["cognito:username"];
-        let email = event.requestContext.authorizer.claims.email;
-
-        responsePromise = (async function(method) {
-            switch(method) {
-                case 'create':
-                    await validate.create(body, authorizedUser, email);
-                    console.log("Create validation passed");
-                    return api.create(body, email);
-                case 'read':
-                    await validate.read(body);
-                    return api.read(body, email);
-                case 'update':
-                    await validate.update(body);
-                    return api.update(body, email);
-                case 'delete':
-                    await validate.delete(body);
-                    return api.delete(body, email);
-                case 'list':
-                    return api.list(email);
-                default:
-                    return Promise.reject({message: "Unrecognized method name ".concat(method)});
-            }
-        })(method);
-    } else {
+    if (!event.httpMethod){
         responsePromise = Promise.reject(`Lambda received an event it did not know how to parse: ${JSON.stringify(event, undefined, 2)}`)
     }
+
+    let method = event.pathParameters.proxy;
+    let body = null;
+    if (event.body) {
+        body = JSON.parse(event.body);
+    }
+    let authorizedUser = event.requestContext.authorizer.claims["cognito:username"];
+    let email = event.requestContext.authorizer.claims.email;
+
+    responsePromise = (async function(method) {
+        switch(method) {
+            case 'create':
+                await validate.create(body, authorizedUser, email);
+                console.log("Create validation passed");
+                return api.create(body, email);
+            case 'read':
+                await validate.read(body);
+                return api.read(body, email);
+            case 'update':
+                await validate.update(body);
+                return api.update(body, email);
+            case 'delete':
+                await validate.delete(body);
+                return api.delete(body, email);
+            case 'list':
+                return api.list(email);
+            default:
+                return Promise.reject({message: "Unrecognized method name ".concat(method)});
+        }
+    })(method);
 
     let response = null;
     try {
